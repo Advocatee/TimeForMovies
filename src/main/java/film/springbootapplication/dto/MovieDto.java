d91f623be810fc37be6a08a44cda6649a1a37c3f
Roman <All.Under.Control@mail.ru>
2020-04-04 18:22:37 +0300

    fix MovieDao

--- src/main/java/film/springbootapplication/dto/MovieDto.java
+++ src/main/java/film/springbootapplication/dto/MovieDto.java
@@ -4,8 +4,6 @@ import film.springbootapplication.model.Genre;
 import film.springbootapplication.model.ProductionCountry;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import springfox.documentation.swagger2.mappers.ModelMapper;
 
 import java.util.Date;
 import java.util.List;
@@ -14,8 +12,6 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 public class MovieDto {
-    @Autowired
-    private ModelMapper mapper;
 
     private String title;
     private Date releaseDate;
