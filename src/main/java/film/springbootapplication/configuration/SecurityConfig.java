b1cd638fd3f54b209c45a2c3ee34ab6b296cded3
Roman <All.Under.Control@mail.ru>
2020-06-12 14:10:31 +0300

    spring security(details service,authenticationProvider)

--- src/main/java/film/springbootapplication/configuration/SecurityConfig.java
+++ src/main/java/film/springbootapplication/configuration/SecurityConfig.java
@@ -1,7 +1,10 @@
 package film.springbootapplication.configuration;
 
+import film.springbootapplication.service.impl.MyUserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,19 +14,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    MyUserDetailsServiceImpl userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("roma").password("{noop}roma").roles("ADMIN");
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("roma").password("$2y$12$4J2kgGpX/BQRPdifsZmJHu8ZJqjvmMEOxNcCsph5NoTcLeesAtxP6").roles("ADMIN")
+                .and()
+                .withUser("user").password("$2y$12$p5ILZBQZz2QrJLFL6luA0OCGLodNN/IwOIoKyJy5Sqxl1M6GjwC8.").roles("USER");
+//        auth.authenticationProvider(authenticationProvider());
+
+    }
+
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/genres**").hasRole("ADMIN")
+                .antMatchers("/movies**").authenticated()
+                .antMatchers("/companies**").hasAnyRole("ADMIN", "USER")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutSuccessUrl("/");
     }
 }
--- src/main/java/film/springbootapplication/controller/GenreController.java
+++ src/main/java/film/springbootapplication/controller/GenreController.java
@@ -7,7 +7,6 @@ import film.springbootapplication.service.GenreService;
 import film.springbootapplication.validator.GenreValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
